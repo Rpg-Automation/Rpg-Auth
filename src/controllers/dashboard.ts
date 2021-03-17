318b913abd3b2301abc5723f24186e875c310ed4
JudeBoy <judegiordano@gmail.com>
2021-03-17 09:52:23 -0400

    Cookie Testing

--- src/controllers/dashboard.ts
+++ src/controllers/dashboard.ts
@@ -17,6 +17,7 @@ router.get("/discord/dash/callback", _pass,
 	async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 		try {
 			const token = await Jwt.Sign(req.user as IJwtPayload);
+			res.cookie("have_a_cookie", "is_for_me");
 			res.redirect(`${config.DASHBOARD_ORIGIN}/auth/${token}`);
 		} catch (e) {
 			next(e);
