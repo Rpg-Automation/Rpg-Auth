49c60bf01dc0ea63f2724c0f1017dd7e2c238890
JudeBoy <judegiordano@gmail.com>
2021-03-18 16:01:10 -0400

    Update Server Headers

--- src/services/server.ts
+++ src/services/server.ts
@@ -10,11 +10,17 @@ import config from "../helpers/config";
 const app: Express = express();
 app.use(cors({
 	//allowedHeaders: "*",
-	allowedHeaders: ["Cookie", "Cookies", "cookie", "cookies", "Origin", "Access-Control-Allow-Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token", "Authorization", "Cookie"],
+	allowedHeaders: ["Cookie", "Cookies", "cookie", "cookies", "Origin", "Access-Control-Allow-Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token", "Authorization"],
 	origin: config.DASHBOARD_ORIGIN,
 	credentials: true,
 	preflightContinue: false
 }));
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", config.DASHBOARD_ORIGIN);
+	res.header("Access-Control-Allow-Credentials", "true");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 app.use(helmet());
 app.use(cookieParser(config.COOKIE_SECRET));
 app.use(express.json());
